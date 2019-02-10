a6d8b8fcb0e29f7f558e8489b8abe515da2ef5fb
root <root@zeus.haunwang.de>
2019-02-10 14:27:22 +0100

    scripts

--- run.sh
+++ run.sh
@@ -3,7 +3,9 @@
 docker run \
     --name dnsmasq \
     -d \
-    -v /opt/dnsmasq:/etc/dnsmasq \
+    -v /docker-data/dnsmasq/dnsmasq.conf:/etc/dnsmasq.conf \
+    -v /docker-data/dnsmasq/conf:/etc/dnsmasq.d \
+    -v /docker-data/dnsmasq/log:/var/log \
     --cap-add=NET_ADMIN \
     --net=host \
     --restart unless-stopped \
